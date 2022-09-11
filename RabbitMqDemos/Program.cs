dd815d6e1fd5e198b47b7efaca7443cb4c9cd749
wmate <wmate@WIKTOR-YOGA>
2022-09-11 12:10:53 +0200

    Add project files.

---
+++ RabbitMqDemos/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+var conn = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port=5672,
+};
+using var channel = conn.CreateConnection();
+using var model = channel.CreateModel();
+
+model.ExchangeDeclare("ex.direct", "direct", true, false);
+model.QueueDeclare("q.info", true, false, false);
+model.QueueDeclare("q.error", true, false, false);
+
+model.QueueBind("q.info", "ex.direct", "info");
+model.QueueBind("q.error", "ex.direct", "error");
+
+model.QueueDelete("q.info");
+model.QueueDelete("queue.error");
+model.ExchangeDelete("ex.direct");
